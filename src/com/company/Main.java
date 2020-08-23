2bb65d2631bdc829f3ac2c5bd0cd90e749190d3f
jasneet6114 <parwindersangha1978@gmail.com>
2020-08-23 23:27:39 +0530

    Calculate simple interest

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the amount of your loan :  ");
+        String p = scanner.nextLine();
+        Integer pInt = Integer.parseInt(p);
+
+        System.out.print("Rate of Interest :");
+        String r = scanner.nextLine();
+        float rInt = Float.parseFloat(r);
+
+
+        System.out.print("Time (Years) :");
+        String t = scanner.nextLine();
+        Integer tInt = Integer.parseInt(t);
+
+
+        double simpleInterest = (pInt * rInt * tInt / 100);
+        System.out.println(simpleInterest);
+
+
+    }
+
+}
